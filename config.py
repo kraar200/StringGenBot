4fc4f7226ba8b6e3cb7b4e9fc243f7ca4c0d81ed
KiMO <109815962+kraar200@users.noreply.github.com>
2023-07-22 13:21:05 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 611122715))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/d8_8q")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevKiMO")
